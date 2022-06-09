36b9ed3efde72898c1b0f8ce65b1babdedd44442
ManuelBenini <manuelbenini1905@gmail.com>
2022-06-09 17:39:48 +0200

    terminato bonus ed unite le due funzioni in una sola

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const app = new Vue({
     disks: [],
     genres: [],
     authors: [],
+    selectedGenre: 'none',
+    selectedAuthor: 'none',
     isLoaded: false
   },
   methods:{
@@ -15,28 +17,41 @@ const app = new Vue({
         console.log('Dischi', this.disks);
         if(this.disks.length === response.data.length){
           this.isLoaded = true;
-          this.getGenresList();
-          this.getAuthorsList();
+          this.getGenresAndAuthors();
         }
       })
     },
-    getGenresList(){
+    getGenresAndAuthors(){
       this.disks.forEach(disk => {
         if (!this.genres.includes(disk.genre)){
           this.genres.push(disk.genre);
         }
-      });
-
-      console.log('Lista generi', this.genres);
-    },
-    getAuthorsList(){
-      this.disks.forEach(disk => {
         if (!this.authors.includes(disk.author)){
           this.authors.push(disk.author);
         }
       });
 
+      console.log('Lista generi', this.genres);
       console.log('Lista autori', this.authors);
+    },
+  },
+  computed: {
+    filteredDisks() {
+      let filteredArray = [];
+      if (this.selectedGenre === 'none') {
+        filteredArray = this.disks;
+      } else {
+        filteredArray = this.disks.filter(disk => {
+          return disk.genre.toLowerCase().includes(this.selectedGenre.toLowerCase());
+        });
+      }
+      if (this.selectedAuthor != 'none') {
+        filteredArray = filteredArray.filter(disk => {
+          return disk.author.toLowerCase().includes(this.selectedAuthor.toLowerCase());
+        });
+      }
+      console.log('array filtro genere e filtro artista', filteredArray);
+      return filteredArray;
     }
   },
   mounted(){
